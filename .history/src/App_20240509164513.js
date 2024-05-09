7ff759295ec88342d186db65dcbb6d11e0694fb5
eyan ingles <eyaningles29@gmail.com>
2024-05-09 16:46:40 +1000

    added in css from bootstrap

---
+++ .history/src/App_20240509164513.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./features/counter/counterSlice";
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+
+function App() {
+const count = useSelector((state) => state.counter.value);
+const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <header >
+        <h1>NFT Marketplace</h1>
+      </header>
+      <Tabs
+      defaultActiveKey="profile"
+      id="justify-tab-example"
+      className="nav-tabs"
+      justify>
+        <Tab
+        eventKey="home"
+        title="List"
+        className='nav'>
+        </Tab>
+        <Tab eventKey="profile"
+        title="Buy"
+        className='nav'
+        >
+        </Tab>
+        <Tab eventKey="longer-tab"
+        title="Mint"
+        className='nav'>
+        </Tab>
+    </Tabs>
+   
+    </div>
+  );
+}
+
+export default App;
