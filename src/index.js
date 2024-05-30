f559c6b7fe7f1d19b0b2383bff5c5082cbe1bb3b
eyan ingles <eyaningles29@gmail.com>
2024-05-30 19:28:54 +1000

    attmept 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import store from './app/store/store';
 import { Provider } from 'react-redux'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
